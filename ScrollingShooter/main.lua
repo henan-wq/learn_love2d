f4dcf3d33993859ac1f09e24a8e0b924edbcd509
henan-wq <nanhe31@gmail.com>
2022-04-04 01:56:57 +0800

    modified:   main.lua

--- ScrollingShooter/main.lua
+++ ScrollingShooter/main.lua
@@ -1,16 +1,26 @@
 debug = true
 
-playerImg = nil -- this is just for storage
+player = { x = 200, y = 710, speed = 150, img = nil } -- this is just for storage
 
 function love.load(arg)
-	playerImg = love.graphics.newImage('assets/plane.png')
+	player.img = love.graphics.newImage('assets/plane.png') 
 	-- we now have an asset ready to be used inside Love
 end
 
 function love.update(dt)
+	-- I always start with an easy way to exit the game
+	if love.keyboard.isDown('escape') then
+		love.event.push('quit')
+	end
+
+	if love.keyboard.isDown('left','a') then
+		player.x = player.x - (player.speed*dt)
+	elseif love.keyboard.isDown('right','d') then
+		player.x = player.x + (player.speed*dt)
+	end
 
 end
 
 function love.draw(dt)
-	love.graphics.draw(playerImg, 100, 100)
+	love.graphics.draw(player.img, player.x, player.y)
 end
